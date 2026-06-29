d3e8e34fbe188b434880432e587aacfa2291b9f3
Mike Taylor <mike@indexdata.com>
2026-06-29 20:53:21 +0100

    In fact, rip out unused proptypes

--- src/settings/some-feature-settings.js
+++ src/settings/some-feature-settings.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Pane } from '@folio/stripes/components';
 import { FormattedMessage } from 'react-intl';
 
 export default class FeatureSettings extends React.Component {
-  static propTypes = {
-    label: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  };
-
   render() {
     return (
       <Pane defaultWidth="fill" fluidContentWidth paneTitle={this.props.label}>
